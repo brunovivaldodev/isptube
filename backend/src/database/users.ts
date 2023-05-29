e8742dfa0ee030250dbace113b51448e4865f312
Bruno Vivaldo <brunolumeca@live.com>
2023-05-29 21:47:12 +0100

    chore : user findByID method created

--- backend/src/database/users.ts
+++ backend/src/database/users.ts
@@ -1,24 +1,34 @@
 import { PrismaClient } from "@prisma/client";
-import {v4 as uuidV4} from "uuid"
+import { v4 as uuidV4 } from "uuid";
 export interface ICreateUser {
-    name: string;
-    email: string;
-    password: string;
+  name: string;
+  email: string;
+  password: string;
 }
 
-
 export class DatabaseUser {
-    readonly prisma = new PrismaClient()
+  readonly prisma = new PrismaClient();
 
-    async create({email,name,password} : ICreateUser ) {
-       return await this.prisma.user.create({data : {email, name,password, id : uuidV4()}})
-    }
+  async create({ email, name, password }: ICreateUser) {
+    return await this.prisma.user.create({
+      data: {
+        email,
+        name,
+        password,
+        id: uuidV4(),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    });
+  }
 
-    async findByEmail(email : string) {
-        const user = this.prisma.user.findFirst({where : {email : email}})
-        return user
-    }
+  async findByEmail(email: string) {
+    const user = await this.prisma.user.findFirst({ where: { email: email } });
+    return user;
+  }
 
-
-
+  async findByID(id: string) {
+    const user = await this.prisma.user.findUnique({ where: { id } });
+    return user;
+  }
 }
