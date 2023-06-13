d11e704353ac090bf74f449bf9518f0a6a20c2f3
Bruno Vivaldo <brunolumeca@live.com>
2023-06-13 21:35:38 +0100

    feat: midea stream done

--- backend/src/routes/mideas.routes.ts
+++ backend/src/routes/mideas.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
-
+import fs from "fs";
+import { resolve } from "path";
 import { progress_middleware } from "./middlewares";
 import uploadConfig from "../config/upload";
 import multer from "multer";
@@ -69,6 +70,39 @@ router.post(
 
     const { count, midea } = await mideaController.findById(id);
     return response.json({ ...midea, count });
+  }),
+
+  router.get("/stream/:id", async function (request, response) {
+    const { id } = request.params;
+
+    const { midea } = await mideaController.findById(id);
+    const path = resolve("uploads/", `${midea?.url}`);
+
+    const stat = fs.statSync(path);
+    const fileSize = stat.size;
+    const range = request.headers.range;
+    if (range) {
+      const parts = range.replace(/bytes=/, "").split("-");
+      const start = parseInt(parts[0], 10);
+      const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+      const chunksize = end - start + 1;
+      const file = fs.createReadStream(path, { start, end });
+      const head = {
+        "Content-Range": `bytes ${start}-${end}/${fileSize}`,
+        "Accept-Ranges": "bytes",
+        "Content-Length": chunksize,
+        "Content-Type": "video/mp4",
+      };
+      response.writeHead(206, head);
+      file.pipe(response);
+    } else {
+      const head = {
+        "Content-Length": fileSize,
+        "Content-Type": "video/mp4",
+      };
+      response.writeHead(200, head);
+      fs.createReadStream(path).pipe(response);
+    }
   })
 );
 
