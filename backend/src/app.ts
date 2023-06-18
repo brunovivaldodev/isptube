330d43d939e4ac0d6cb9c3d8d125d979564eaf66
Bruno Vivaldo <brunolumeca@live.com>
2023-06-18 14:52:28 +0100

    chore: express async errors added

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,37 +1,39 @@
-import express, { NextFunction, Request, Response } from "express"
-import {createServer} from "http"
-import {Server} from "socket.io"
-import cors from "cors"
+import express, { NextFunction, Request, Response } from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+import 'express-async-errors'
+
 import routes from "./routes";
 import AppError from "./errors/appError";
-import path from "path"
+import path from "path";
 export const app = express();
-const httpServer = createServer(app)
+const httpServer = createServer(app);
 const io = new Server(httpServer, {
-  cors : {
-    origin : "*",
-    credentials : false
-  }
-})
-app.use(cors())
+  cors: {
+    origin: "*",
+    credentials: false,
+  },
+});
+app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname,"..", "uploads")))
+app.use(express.static(path.join(__dirname, "..", "uploads")));
 
-app.use(routes)
+app.use(routes);
 
-app.use((err:Error,resquest :Request,response :Response,next:NextFunction)=>{
-  if (err instanceof AppError){
-    return response.status(err.statusCode).json({
-      message : err.message
-    })
+app.use(
+  (err: Error, resquest: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: "error",
+      message: `Internal Error Server -${err.stack}`,
+    });
   }
+);
 
-  return response.status(500).json({
-    status : 'error',
-    message : `Internal Error Server -${err.stack}`
-  })
-})
-
-io.on("connection",(socket)=> console.log(`someone connected: ${socket.id}`))
-
-
+io.on("connection", (socket) => console.log(`someone connected: ${socket.id}`));
