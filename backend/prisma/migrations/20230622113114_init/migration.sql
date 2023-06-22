a2ffb1107f3ac192841d5b4e5e96bdd27a503f91
Bruno Vivaldo <brunolumeca@live.com>
2023-06-22 17:28:38 +0100

    chore: create playlist schema

--- backend/prisma/migrations/20230622113114_init/migration.sql
+++ backend/prisma/migrations/20230622113114_init/migration.sql
@@ -34,6 +34,19 @@ CREATE TABLE "mideas" (
 );
 
 -- CreateTable
+CREATE TABLE "playlists" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "visibility" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "user_id" TEXT NOT NULL,
+
+    CONSTRAINT "playlists_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "comments" (
     "id" TEXT NOT NULL,
     "message" TEXT NOT NULL,
@@ -45,14 +58,35 @@ CREATE TABLE "comments" (
     CONSTRAINT "comments_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "_MideaToPlaylist" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
 
+-- CreateIndex
+CREATE UNIQUE INDEX "_MideaToPlaylist_AB_unique" ON "_MideaToPlaylist"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_MideaToPlaylist_B_index" ON "_MideaToPlaylist"("B");
+
 -- AddForeignKey
 ALTER TABLE "mideas" ADD CONSTRAINT "mideas_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "playlists" ADD CONSTRAINT "playlists_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "comments" ADD CONSTRAINT "comments_midea_id_fkey" FOREIGN KEY ("midea_id") REFERENCES "mideas"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "comments" ADD CONSTRAINT "comments_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_MideaToPlaylist" ADD CONSTRAINT "_MideaToPlaylist_A_fkey" FOREIGN KEY ("A") REFERENCES "mideas"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_MideaToPlaylist" ADD CONSTRAINT "_MideaToPlaylist_B_fkey" FOREIGN KEY ("B") REFERENCES "playlists"("id") ON DELETE CASCADE ON UPDATE CASCADE;
