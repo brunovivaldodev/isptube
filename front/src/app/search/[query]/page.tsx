57be6ff13a298558b1512325f60488f01af5a238
Bruno Vivaldo <brunolumeca@live.com>
2023-06-23 16:22:51 +0100

    chore: search list add playlists

--- front/src/app/search/[query]/page.tsx
+++ front/src/app/search/[query]/page.tsx
@@ -1,10 +1,12 @@
 import { Mideas } from "@/app/home/page";
+import { Playlists } from "@/app/playlists/page";
 import { Community } from "@/components/community";
+import { FilterSearch } from "@/components/filterSearch";
 import { Header } from "@/components/header";
 import { getUser } from "@/contexts/auth";
 import { User } from "@/contexts/profile";
 import { api } from "@/lib/api";
-import { SlidersHorizontal } from "lucide-react";
+import { ListVideo } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -21,11 +23,13 @@ export default async function Search({ params: { query } }: Params) {
     },
   });
 
-  const { mideas, users } = response.data;
+  const { mideas, users, playlists } = response.data;
 
   const mideasFetched: Mideas[] = mideas;
 
   const usersFetched: User[] = users;
+
+  const playlistFetched: Playlists[] = playlists;
 
   return (
     <>
@@ -33,10 +37,7 @@ export default async function Search({ params: { query } }: Params) {
 
       <div className="flex mt-6">
         <section className="flex-1">
-          <div className="flex space-x-4 my-2">
-            <SlidersHorizontal />
-            <p>Filtrar</p>
-          </div>
+          <FilterSearch />
           <div className="border-b my-4"></div>
           {usersFetched.length === 0 ? (
             <div className="flex flex-1 items-center justify-center p-16">
@@ -63,7 +64,12 @@ export default async function Search({ params: { query } }: Params) {
                 </div>
 
                 <div className="mx-12">
-                  <p className="my-2">{user.name}</p>
+                  <Link
+                    href={`/profile/${user.id}`}
+                    className="font-light	text-sm"
+                  >
+                    {user.name}
+                  </Link>
                   <p>{user.midea.length} Mideas</p>
                   <p>{user.description}</p>
                 </div>
@@ -86,9 +92,9 @@ export default async function Search({ params: { query } }: Params) {
             )}
             {mideasFetched.map((midea) => {
               return (
-                <div key={midea.id} className="w-64">
+                <div key={midea.id} className="flex space-x-8">
                   <Link href={`/midea/${midea.id}`}>
-                    <div className={`h-32 relative overflow-hidden`}>
+                    <div className={`w-64 h-32 relative overflow-hidden`}>
                       <Image
                         src={`${api.getUri()}/${midea.cover_url}`}
                         alt=""
@@ -100,19 +106,73 @@ export default async function Search({ params: { query } }: Params) {
                     </div>
                   </Link>
 
-                  <div className="flex flex-col">
-                    <span className="font-normal mt-1">{midea.name}</span>
-                    <div className="flex place-content-between">
-                      <Link
-                        href={`/profile/${midea.user.id}`}
-                        className="font-light	text-sm"
-                      >
-                        {midea.user.name}
-                      </Link>
-                      <span className="font-light text-sm	">{`${new Date(
-                        midea.created_at
-                      ).toDateString()}`}</span>
-                    </div>
+                  <div className="flex flex-col space-y-4">
+                    <span className="font-bold mt-1">{midea.name}</span>
+                    <span className="font-normal mt-1">
+                      {midea.description}
+                    </span>
+                    <Link
+                      href={`/profile/${midea.user.id}`}
+                      className="font-light	text-sm"
+                    >
+                      {midea.user.name}
+                    </Link>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+
+          <div className="border-b my-4"></div>
+          <div className="my-2">Playlists</div>
+
+          <div className="flex flex-col gap-6 content-around">
+            {playlistFetched.length === 0 ? (
+              <div className="flex flex-1 items-center justify-center p-16">
+                <p className="w-[360px] text-center leading-relaxed">
+                  Nenhuma playlist encontrada
+                </p>
+              </div>
+            ) : (
+              <></>
+            )}
+            {playlistFetched.map((playlist) => {
+              return (
+                <div key={playlist.id} className="flex space-x-8">
+                  <div
+                    className={`h-32 w-64 relative overflow-hidden bg-white`}
+                  >
+                    <Link href={`/playlists/${playlist.id}`}>
+                      <Image
+                        src={
+                          playlist.mideas.length === 0
+                            ? `/playlist.png`
+                            : `${api.getUri()}/${playlist.mideas[0].cover_url}`
+                        }
+                        alt=""
+                        fill
+                      />
+                      <div className="flex bg-zinc-800 m-2 absolute bottom-0 left-0">
+                        <ListVideo />
+                        <span className="text-white ml-1">
+                          {" "}
+                          {playlist.mideas.length}
+                        </span>
+                      </div>
+                    </Link>
+                  </div>
+
+                  <div className="flex flex-col flex-1 place-content-between">
+                    <p className="font-normal mt-1">{playlist.name}</p>
+                    <p className="font-normal mt-1">{playlist.description}</p>
+
+                    <Link href={`/playlists/${playlist.id}`}>
+                      VER PLAYLIST COMPLETA
+                    </Link>
+
+                    <p className="font-light text-sm	mt-1">{`${new Date(
+                      playlist.created_at
+                    ).toDateString()}`}</p>
                   </div>
                 </div>
               );
