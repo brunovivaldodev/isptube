b92ba70b3af83575dfd5efb21b9a12af856d094b
Bruno Vivaldo <brunolumeca@live.com>
2023-06-24 13:09:02 +0100

    chore: socet io config

--- front/src/lib/api.ts
+++ front/src/lib/api.ts
@@ -1,7 +1,14 @@
 import axios from "axios";
+import { io } from "socket.io-client";
+
+const baseURL = "http://localhost:3333";
+
+export const socket = io(baseURL, {
+  autoConnect: false,
+});
 
 export const api = axios.create({
-  baseURL: "http://localhost:3333",
+  baseURL,
 });
 
 export const nextApi = "http://localhost:3000";
