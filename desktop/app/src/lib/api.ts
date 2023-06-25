8cb770aeedbddf4915e824db4792590f22f666a8
Bruno Vivaldo <brunolumeca@live.com>
2023-06-25 19:40:12 +0100

    chore: desktop done

--- desktop/app/src/lib/api.ts
+++ desktop/app/src/lib/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
+const baseURL = "http://localhost:3333";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3333",
+  baseURL,
 });
 
 export const nextApi = "http://localhost:3000";
--- desktop/main.js
+++ desktop/main.js
@@ -1,11 +1,20 @@
 const { app, BrowserWindow } = require("electron");
-
+const path = require("path");
+const { ipcMain } = require("electron/main");
+const { setStore } = require("./settings");
 function createMainWindow() {
   const mainWindow = new BrowserWindow({
     title: "ISPTUBE",
     width: 1000,
     height: 600,
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: true,
+      preload: path.join(__dirname, "preload.js"),
+    },
   });
+
+  mainWindow.webContents.openDevTools();
 
   mainWindow.loadURL("http://localhost:3000/");
 }
@@ -24,4 +33,11 @@ app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
     app.quit();
   }
+});
+
+ipcMain.on("key:set", async (e, opt) => {
+  setStore(`${email}`, {
+    email,
+    password,
+  });
 });
