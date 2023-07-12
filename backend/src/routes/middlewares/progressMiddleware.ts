15b8e39c5d6657300185f8543101a7361dbc5327
Bruno Vivaldo <brunolumeca@live.com>
2023-07-12 21:04:57 +0100

    chore: socket io send upload progress done

--- backend/src/routes/middlewares/progressMiddleware.ts
+++ backend/src/routes/middlewares/progressMiddleware.ts
@@ -1,21 +1,26 @@
 import { NextFunction, Request, Response } from "express";
+import { io } from "../../app";
 
-export function progress_middleware(req : Request, res : Response, next : NextFunction){
-    let progress = 0;
-    const file_size = req.headers["content-length"];
+export function progress_middleware(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  let progress = 0;
+  const file_size = req.headers["content-length"];
 
-    if(!file_size) {
-        throw new Error("sdds")
-    }
-    
-    // set event listener
-    req.on("data", (chunk) => {
-        progress += chunk.length;
-        const percentage = (progress / parseInt(file_size)) * 100;
-        
-        console.log(percentage)
-    });
+  if (!file_size) {
+    throw new Error("sdds");
+  }
 
-    // invoke next middleware
-    next();
+  // set event listener
+  req.on("data", (chunk) => {
+    progress += chunk.length;
+    const percentage = (progress / parseInt(file_size)) * 100;
+
+    io.emit("upload", percentage);
+  });
+
+  // invoke next middleware
+  next();
 }
